3bb1671dd38676955f99d65224f61302f1bfcc5e
Jure Smolar <jure.smolar@gmail.com>
2024-12-03 13:04:36 +0100

    General day template

---
+++ Aoc2024Lean/Template.lean
@@ -0,0 +1,20 @@
+import Batteries.Data.List.Basic
+import Aoc2024Lean.Utils
+
+open Utils List
+
+namespace Day00
+
+def solve1 (input : String) :=
+  0
+
+def solve2 (input : String) :=
+  0
+
+def run : IO Unit := do
+  let input ← readInput 0
+  let left_ids ←
+  IO.println <| solve1 input
+  IO.println <| solve2 input
+
+end Day00
