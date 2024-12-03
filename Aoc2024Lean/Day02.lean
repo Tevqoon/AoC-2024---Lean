f03bd03ee934e63084441c38c2ddb1045d7f1605
Jure Smolar <jure.smolar@gmail.com>
2024-12-03 13:04:27 +0100

    Day 2

---
+++ Aoc2024Lean/Day02.lean
@@ -0,0 +1,43 @@
+import Batteries.Data.List.Basic
+import Aoc2024Lean.Utils
+
+open Utils List
+
+namespace Day02
+
+def safe (report : List Int) : Bool :=
+  let diffs := zipWith (fun x y => x - y) report (tail! report)
+  all diffs (fun x => 1 ≤ x && x ≤ 3)
+  || all diffs (fun x => (-3 : Int) ≤ x && x ≤ (-1 : Int))
+
+def test := "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
+
+def solve1 (input : String) :=
+  let report := map parseInts (lines input)
+  filter safe report
+  |> length
+
+#eval solve1 test
+
+def dampener (diffs : List Int) : List (List Int) :=
+  map (fun i => eraseIdx diffs i) (range <| length diffs)
+
+def solve2 (input : String) :=
+  let report := map parseInts (lines input)
+  filter (fun rep => any (dampener rep) safe) report
+  |> length
+
+#eval solve2 test
+
+def run : IO Unit := do
+  let input ← readInput 2
+  let left_ids ←
+  IO.println <| solve1 input
+  IO.println <| solve2 input
+
+end Day02
--- Main.lean
+++ Main.lean
@@ -1,4 +1,5 @@
 import Aoc2024Lean.Day01
+import Aoc2024Lean.Day02
 
 def printUsage : IO UInt32 := do
   IO.println "Usage: aoc-2024-lean [day_number]"
@@ -6,19 +7,21 @@ def printUsage : IO UInt32 := do
   IO.println "  aoc-2024-lean 1    # Run only day 1"
   pure 0
 
-def runDay (day : Nat) : IO UInt32 := do   
+def runDay (day : Nat) : IO UInt32 := do
   match day with
-  | 1 => do Day01.run 
+  | 1 => do Day01.run
+            pure 0
+  | 2 => do Day02.run
             pure 0
   | _ => do IO.println "Invalid day number!"
             pure 1
 
 def main (args : List String) : IO UInt32 := do
   match args with
-  | [n] => 
+  | [n] =>
     match n.toNat? with
     | some day => runDay day
-    | none => 
+    | none =>
       IO.println "Invalid day number!"
       printUsage
   | _ =>
